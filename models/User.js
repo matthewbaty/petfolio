f3457e3348ee74c0a51ea5371e58f0a4f3b83914
rm2023 <120582189+rm2023@users.noreply.github.com>
2023-05-02 20:09:42 -0400

    added hooks

--- models/User.js
+++ models/User.js
@@ -27,8 +27,17 @@ User.init(
     
   },
   {
+    hooks: {
+        beforeCreate: async (newUserData) => {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        beforeUpdate: async (updatedUserData) => {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        },
+    },
     sequelize,
-    timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'user',
