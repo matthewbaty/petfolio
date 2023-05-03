b828380eaebec80c2e1e6b5bd541b5ac6434ea0c
Matthew Baty <mattbaty@outlook.com>
2023-05-02 21:29:12 -0400

    added index seeds file, added handlebars to server.js file

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//handlebars helpers
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
@@ -22,6 +27,7 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
