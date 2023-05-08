f2578daf5fddb695c522b8545de2a3e32222c0ec
Matthew Baty <mattbaty@outlook.com>
2023-05-08 19:36:28 -0400

    changed pet update route to use try catch

--- controllers/api/petRoutes.js
+++ controllers/api/petRoutes.js
@@ -15,25 +15,26 @@ router.post('/', withAuth, async (req, res) => {
     }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, async (req, res) => {
     // update a tag's name by its `id` value
-    Pet.update(req.body, {
-      where: {
-        pet_id: req.params.pet_id
-      }
-    })
-    .then(petData => {
-      if (!petData) {
-        res.status(404).json({message: 'No pet found with this id!'});
-        return;
-      }
-      res.json(petData);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-  });
+    try {
+        const petData = await Pet.update({
+            ...req.body,
+            where: {
+                pet_id: req.params.pet_id
+            }
+        })
+            .then(petData => {
+                if (!petData) {
+                    res.status(404).json({ message: 'No pet found with this id!' });
+                    return;
+                }
+                res.status(200).json(petData);
+            })
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 router.delete('/:id', withAuth, async (req, res) => {
     try {
